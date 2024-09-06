1980d5cdef1b8626ba6e6e91523ef9bdf80fe30a
Bhashkar Kumar <153943769+BhashkarKumar2@users.noreply.github.com>
2024-09-06 12:57:16 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -134,12 +134,7 @@ const NotificationDashboard = () => {
                 </button>
               </div>
               {/* Progress Bar */}
-              <div className="w-full bg-gray-200 rounded-full h-2.5">
-                <div
-                  className="bg-blue-600 h-2.5 rounded-full"
-                  style={{ width: `${tender.progress}%` }}
-                ></div>
-              </div>
+              
             </div>
           ))}
         </div>
